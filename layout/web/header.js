145b7d40f431cc40a901bf39fad6d2c6902365e6
nadim <nadim.sheikh.07@gmail.com>
2021-08-07 23:34:29 +0530

    add nav scroll

--- layout/web/header.js
+++ layout/web/header.js
@@ -1,8 +1,33 @@
+import React from "react"
 import { Container, Nav, Navbar, NavDropdown } from "react-bootstrap"
 
 const WebHeader = () => {
+
+    let listener = null
+    const [scrollState, setScrollState] = React.useState("top")
+
+    React.useEffect(() => {
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+        listener = document.addEventListener("scroll", _e => {
+            var scrolled = document.scrollingElement.scrollTop
+            console.warn('scrollState', scrollState)
+            if (scrolled >= 120) {
+                if (scrollState !== "down") {
+                    setScrollState("down")
+                }
+            } else {
+                if (scrollState !== "top") {
+                    setScrollState("top")
+                }
+            }
+        })
+        return () => {
+            document.removeEventListener("scroll", listener)
+        }
+    }, [scrollState])
+
     return (
-        <Navbar bg="light" expand="lg" fixed="top" >
+        <Navbar bg={scrollState == "down" ? "primary" : "light"} variant="dark" expand="lg" fixed="top" >
             <Container fluid>
                 <Navbar.Brand href="#home">{process.env.NEXT_PUBLIC_APP_NAME}</Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
