d2309f12e02849847e29469db151d150733a2399
nadim <nadim.sheikh.07@gmail.com>
2021-08-07 23:09:18 +0530

    add home slider

---
+++ component/slider/homeBanner.js
@@ -0,0 +1,57 @@
+import Image from 'next/image'
+import { Carousel } from "react-bootstrap"
+import useWindowDimensions from '../../hooks/useWindowDimensions';
+
+const HomeBanner = () => {
+    const { width } = useWindowDimensions();
+    console.warn('width', width)
+    return (
+        <Carousel variant="dark">
+            <Carousel.Item>
+                <Image
+                    className="d-block w-100"
+                    src="/images/8D1A9232.JPG"
+                    alt="First slide"
+                    height="500"
+                    width={width}
+
+
+                />
+                <Carousel.Caption>
+                    <h5>First slide label</h5>
+                    <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+                <Image
+                    className="d-block w-100"
+                    src="/images/8D1A9233.JPG"
+                    alt="Second slide"
+                    height="500"
+                    width={width}
+
+                />
+                <Carousel.Caption>
+                    <h5>Second slide label</h5>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+                <Image
+                    className="d-block w-100"
+                    src="/images/8D1A9237.JPG"
+                    alt="Third slide"
+                    height="500"
+                    width={width}
+
+                />
+                <Carousel.Caption>
+                    <h5>Third slide label</h5>
+                    <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+                </Carousel.Caption>
+            </Carousel.Item>
+        </Carousel>
+    )
+}
+
+export default HomeBanner
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,10 @@
+import HomeBanner from '../component/slider/homeBanner'
 import withWebLayout from '../layout/web/withWebLayout'
 import styles from '../styles/Home.module.scss'
 const Home = () => {
   return (
     <div className={styles.container}>
-      <h1>Home</h1>
+      <HomeBanner />
     </div>
 
   )
