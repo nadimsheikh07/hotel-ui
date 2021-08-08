afd3b5ac8109390633410be362366d099816ba69
nadim <nadim.sheikh.07@gmail.com>
2021-08-08 19:05:58 +0530

    add

--- pages/about.js
+++ pages/about.js
@@ -1,9 +1,18 @@
+import { Container } from 'react-bootstrap'
+import PageHeader from '../component/modules/pageHeader'
+import HomeParallax from '../component/modules/parallax/homeParallax'
 import withWebLayout from '../layout/web/withWebLayout'
 const About = () => {
   return (
-    <div>
-      <h1>About</h1>
-    </div>
+    <section>
+      <PageHeader title="About Us" />
+      <Container>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga aliquid. Atque dolore esse veritatis iusto eaque perferendis non dolorem fugiat voluptatibus vitae error ad itaque inventore accusantium tempore dolores sunt.
+        </p>
+      </Container>
+      <HomeParallax />
+    </section>
   )
 }
 
