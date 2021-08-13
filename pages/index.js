61ca594692a3cc98f7cb24d6f3489476541ca622
nadim <nadim.sheikh.07@gmail.com>
2021-08-13 23:24:03 +0530

    add home about

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import { AboutDetails } from '../component/modules/about/aboutDetails'
 import { HomeParallax } from '../component/modules/parallax'
 import { ShortServices } from '../component/modules/services'
 import { HomeBanner } from '../component/slider'
@@ -6,6 +7,7 @@ const Home = () => {
   return (
     <section>
       <HomeBanner />
+      <AboutDetails />
       <ShortServices />
       <HomeParallax />
     </section>
