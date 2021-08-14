fea74a4b6ad9078252d001a07e4e08ff905dd6f3
nadim <nadim.sheikh.07@gmail.com>
2021-08-14 11:27:36 +0530

    add navbar module scss

--- layout/web/header.js
+++ layout/web/header.js
@@ -1,6 +1,7 @@
 import React from "react"
 import Link from "next/link";
 import { Container, Nav, Navbar, NavDropdown } from "react-bootstrap"
+import styles from '../../styles/layout/web/Header.module.scss'
 const WebHeader = () => {
 
     let listener = null
@@ -27,37 +28,37 @@ const WebHeader = () => {
     }, [scrollState])
 
     return (
-        <Navbar bg={scrollState == "top" ? "light" : "dark"} variant={scrollState == "top" ? "light" : "dark"} expand="lg" fixed="top" >
+        <Navbar className={scrollState == "top" ? styles.navbarlight : styles.navbardark} expand="lg" fixed="top" >
             <Container fluid>
-                <Navbar.Brand href="#home">{process.env.NEXT_PUBLIC_APP_NAME}</Navbar.Brand>
+                <Navbar.Brand className={styles.navbarbrand}>{process.env.NEXT_PUBLIC_APP_NAME}</Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
 
                 <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
                     <Nav className="ml-auto">
                         <Link href="/" passHref>
-                            <Nav.Link >Home</Nav.Link>
+                            <Nav.Link className={styles.navlink}>Home</Nav.Link>
                         </Link>
                         <Link href="/rooms" passHref>
-                            <Nav.Link>Rooms</Nav.Link>
+                            <Nav.Link className={styles.navlink}>Rooms</Nav.Link>
                         </Link>
                         <Link href="/services" passHref>
-                            <Nav.Link>Services</Nav.Link>
+                            <Nav.Link className={styles.navlink}>Services</Nav.Link>
                         </Link>
                         <Link href="/about" passHref>
-                            <Nav.Link>About</Nav.Link>
+                            <Nav.Link className={styles.navlink}>About</Nav.Link>
                         </Link>
                         <Link href="/blogs" passHref>
-                            <Nav.Link>Blogs</Nav.Link>
+                            <Nav.Link className={styles.navlink}>Blogs</Nav.Link>
                         </Link>
                         <Link href="/contact" passHref>
-                            <Nav.Link>Contact</Nav.Link>
+                            <Nav.Link className={styles.navlink}>Contact</Nav.Link>
                         </Link>
-                        <NavDropdown title="Account" id="basic-nav-dropdown">
-                            <NavDropdown.Item href="#logout">Register</NavDropdown.Item>
-                            <NavDropdown.Item href="#logout">Login</NavDropdown.Item>
-                            <NavDropdown.Item href="#logout">My Account</NavDropdown.Item>
+                        <NavDropdown align="end" className={styles.dropdowntoggle} title="Account" id="basic-nav-dropdown">
+                            <NavDropdown.Item className={styles.dropdownitem} href="#logout">Register</NavDropdown.Item>
+                            <NavDropdown.Item className={styles.dropdownitem} href="#logout">Login</NavDropdown.Item>
+                            <NavDropdown.Item className={styles.dropdownitem} href="#logout">My Account</NavDropdown.Item>
                             <NavDropdown.Divider />
-                            <NavDropdown.Item href="#logout">Logout</NavDropdown.Item>
+                            <NavDropdown.Item className={styles.dropdownitem} href="#logout">Logout</NavDropdown.Item>
                         </NavDropdown>
                     </Nav>
                 </Navbar.Collapse>
