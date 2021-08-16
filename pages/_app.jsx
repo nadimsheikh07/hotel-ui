af427d0623c164abb7223e166992bd3a54f5c1ee
nadim <nadim.sheikh.07@gmail.com>
2021-08-16 18:20:18 +0530

    add redux

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,14 +1,23 @@
+/* eslint-disable react/prop-types */
 import React from 'react';
 import '../styles/globals.scss';
+import { Provider } from 'react-redux';
+import withRedux from "next-redux-wrapper";
+import store from '../redux/store';
 
 const MyApp = ({ Component, pageProps }) => {
   const Layout = Component.Layout ? Component.Layout : React.Fragment;
-
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <Provider store={store}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </Provider>
   );
 };
 
-export default MyApp;
+//makeStore function that returns a new store for every request
+const makeStore = () => store;
+
+//withRedux wrapper that passes the store to the App Component
+export default withRedux(makeStore)(MyApp);
