560344465a5b373610ca7783b5a0c6cac9dbd420
nadim <nadim.sheikh.07@gmail.com>
2021-08-16 10:29:03 +0530

    update eslint

--- pages/about.jsx
+++ pages/about.jsx
@@ -8,10 +8,13 @@ const About = () => (
   <section>
     <PageHeader title="About Us" />
     <AboutDetails />
-    <AboutText title="The shelter is the Outcome of the Dream" details={[
-      "Broker ipsum dolor sit amet, consectetuer luxury elit. Carrara marble commodo ligula eget dolor cum sociis cozy penatibus et magnis.",
-      "Ochean de View extends along a private, quiet and beautiful tropical beach. Stay away from the crowd and enjoy the beauty and lust of Cancun with us."
-    ]} />
+    <AboutText
+      title="The shelter is the Outcome of the Dream"
+      details={[
+        'Broker ipsum dolor sit amet, consectetuer luxury elit. Carrara marble commodo ligula eget dolor cum sociis cozy penatibus et magnis.',
+        'Ochean de View extends along a private, quiet and beautiful tropical beach. Stay away from the crowd and enjoy the beauty and lust of Cancun with us.',
+      ]}
+    />
     <HomeParallax />
   </section>
 );
