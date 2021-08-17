4b72bb8d3bd56408c84ef30f7292bf9f76fe4022
nadim <nadim.sheikh.07@gmail.com>
2021-08-17 17:47:00 +0530

    add redux counter

--- pages/blogs.jsx
+++ pages/blogs.jsx
@@ -1,11 +1,33 @@
+/* eslint-disable react/prop-types */
 import React from 'react';
 import { PageHeader } from '../component/modules/header';
 import withWebLayout from '../layout/web/withWebLayout';
+import { connect } from "react-redux";
+import { Button } from 'react-bootstrap';
+const Blogs = (props) => {
+  console.log('props', props)
+  const { value } = props.counter
 
-const Blogs = () => (
-  <section>
-    <PageHeader title="Blogs" />
-  </section>
-);
+  const counter = (type) => {
+    if (type) {
+      props.dispatch({ type: "INCREMENT_COUNTER" });
+    } else {
+      props.dispatch({ type: "DECREMENT_COUNTER" });
+    }
+  }
 
-export default withWebLayout(Blogs);
+  React.useEffect(() => {
+    counter()
+  }, [])
+  return (
+    <section>
+      <PageHeader title="Blogs" />
+
+      <Button onClick={() => counter(true)}>+</Button>
+      {value}
+      <Button onClick={() => counter(false)}>-</Button>
+    </section>
+  )
+}
+
+export default connect((state) => state)(withWebLayout(Blogs));
--- redux/store.jsx
+++ redux/store.jsx
@@ -1,18 +1,18 @@
 import { applyMiddleware, createStore } from 'redux'
 import { createWrapper } from 'next-redux-wrapper'
 import rootReducer from './reducers/rootReducer';
+import thunkMiddleware from "redux-thunk";
 
 const bindMiddleware = (middleware) => {
-    if (process.env.NODE_ENV !== 'production') {
-        const { composeWithDevTools } = require('redux-devtools-extension')
-        return composeWithDevTools(applyMiddleware(...middleware))
+    if (process.env.NODE_ENV !== "production") {
+        const { composeWithDevTools } = require("redux-devtools-extension");
+        return composeWithDevTools(applyMiddleware(...middleware));
     }
-    return applyMiddleware(...middleware)
+    return applyMiddleware(...middleware);
 }
 
-export const makeStore = (context) => { 
-    const store = createStore(rootReducer, bindMiddleware([]))
-    return store
+export const makeStore = () => {
+    return createStore(rootReducer, bindMiddleware([thunkMiddleware]));
 }
 
 export const wrapper = createWrapper(makeStore, { debug: true })
