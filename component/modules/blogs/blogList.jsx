840decd4a1e43513f3fcac1d08452251df18929b
nadim <nadim.sheikh.07@gmail.com>
2021-08-20 23:08:45 +0530

    bind blog

--- component/modules/blogs/blogList.jsx
+++ component/modules/blogs/blogList.jsx
@@ -2,30 +2,27 @@ import React from 'react';
 import { Col, ListGroup, Row } from 'react-bootstrap';
 import Image from 'next/image';
 import styles from '../../../styles/modules/blogs/BlogList.module.scss';
+import { useSelector } from 'react-redux';
 
-export const BlogList = () => (
-  <ListGroup className={styles.group}>
-    <ListGroup.Item className={styles.item}>
-      <Row>
-        <Col md={2}>
-          <Image width="100" height="100" src="/images/banner1.png" alt="blog 1" />
-        </Col>
-        <Col md={10}>
-          <h5 className={styles.title}>Title</h5>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga aliquid. Atque dolore esse veritatis iusto eaque</p>
-        </Col>
-      </Row>
-    </ListGroup.Item>
-    <ListGroup.Item className={styles.item}>
-      <Row>
-        <Col md={2}>
-          <Image width="100" height="100" src="/images/banner1.png" alt="blog 1" />
-        </Col>
-        <Col md={10}>
-          <h5 className={styles.title}>Title</h5>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga aliquid. Atque dolore esse veritatis iusto eaque</p>
-        </Col>
-      </Row>
-    </ListGroup.Item>
-  </ListGroup>
-);
+export const BlogList = () => {
+  const blogs = useSelector(state => state.blogs.data)
+  return (
+    <ListGroup className={styles.group}>
+      {blogs && blogs.map((blog, index) => {
+        return (
+          <ListGroup.Item key={`blogFooter${index}`} className={styles.item}>
+            <Row>
+              <Col md={2}>
+                <Image width="100" height="100" src={blog.image} alt={blog.title} />
+              </Col>
+              <Col md={10}>
+                <h5 className={styles.title}>{blog.title}</h5>
+                <p>{blog.description}</p>
+              </Col>
+            </Row>
+          </ListGroup.Item>
+        )
+      })}
+    </ListGroup>
+  )
+};
